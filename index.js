d712e49a4116367123b4362d57e49f40e193bf53
Brittany  Arsi <brittmacarsi@icloud.com>
2017-06-19 15:03:09 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+// console.log("hello there!");
+var loremIpsum = require('lorem-ipsum');
+
+const express = require('express');
+const app = express();
+
+
+
+
+
+app.get('/lorem/:number', function (req, res)
+        {
+  res.send( loremIpsum ({
+      count: req.params.number
+      , units: 'paragraphs'
+    , format: 'html'
+})  );
+});
+
+app.listen(3000, function () {
+});
